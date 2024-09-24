7ff80242c84bf0e2b2bd9b488bbb9223655a3eaf
sogzy <emmanuelar35@gmail.com>
2024-09-25 00:24:08 +0100

    added a missing key

--- src/components/ContactContent.jsx
+++ src/components/ContactContent.jsx
@@ -7,8 +7,8 @@ const ContactContent = () => {
     <div className="contact-wrapper mx-auto py-3 mt-4 h-full w-[100%]">
       <h1 className="text-[30px] font-bold py-2 border-b px-4">Intro Call</h1>
       <div className="links flex flex-col gap-4 px-4 mt-6">
-        {contacts.map((contact) => {
-          return <ContactCard {...contact} />;
+        {contacts.map((contact, index) => {
+          return <ContactCard key={index} {...contact} />;
         })}
       </div>
     </div>
